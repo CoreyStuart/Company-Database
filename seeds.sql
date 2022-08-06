692c21ab77a060f2eedbaae52b1509cfc8be7199
Corey Stuart <c.stuart21@yahoo.com>
2022-08-05 18:15:57 -0700

    Original set up of schema, file set up

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (departments.dept_name)
+VALUES 
+("Bartenders"),
+("Barbacks"),
+("Bottle Service"),
+("Security"),
+("Bussers");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
